0bf987ce08997c3fc834fa9b52f8541c51b35c15
MunchkinCat <gabrieljason.clow@calbaptist.edu>
2016-10-10 18:26:23 -0700

    Add files via upload

--- TestDealer.java
+++ TestDealer.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestDealer {
 
     @Test
@@ -21,8 +23,33 @@ public class TestDealer {
         d.serializeToFile();
 
         Inventory i = d.deserializeFromFile();
-        assertEquals("errors getting model", "Ford", i.getMakeModel());
+
+        Vehicle vOne = d.dealerInventory.getVehicle(0);
+        Truck v1 = (Truck) vOne;
+        assertEquals("Error desiaralize file to inventory", "Ford", v1.getMakeModel());
+        assertEquals("Error desiaralize file to inventory", 2015, v1.getYear());
+        assertEquals("Error desiaralize file to inventory", 35_000, v1.getRetailPrice(), 0);
+
     }
 
-}
+    @Test
+    public void testObjectWasSerialized() throws IOException {
+        //given
+        System.out.println("Testing test object was serialized");
+        Dealer d = new Dealer("https://raw.githubusercontent.com/kyungsooim/TestData/master/data/SeptemberInventory.txt");
+        Vehicle v = new Vehicle("Chevy Silverado", 2, 4, true);
 
+
+        //when
+        d.serializeToFile();
+        d.deserializeFromFile();
+        Vehicle check = d.dealerInventory.getVehicle(9);
+
+        //then
+
+        assertEquals("Error serializing to makeModel", v.getMakeModel(), check.getMakeModel());
+        assertEquals("Error serializing to year", v.getYear(), check.getYear());
+        assertEquals("Error serializing to retailprice", v.getRetailPrice(), check.getRetailPrice(), 0);
+
+    }
+}
